c8a742b5b3a9ca0c9954e50a65103ceb18a15c5e
bohorquezitos24 <nbohorquez24@misena.edu.co>
2020-12-04 22:01:21 -0500

    se realiza el primer punto quiz

---
+++ quiz.js
@@ -0,0 +1,35 @@
+console.log("PRUEBA QUIZ PRACTICO");
+
+function Tipo_motor() {
+    var x = Tipo_motor (0, 1, 2, 3, 4);
+    alert("No hay un valor establecido para el tipo de bomba"+ Tipo_motor)
+}
+
+function Tipo_motor() {
+    return 0; 
+    var user = prompt("no hay un valor establecido par este tipo de bomba");
+    alert(user)
+    alert("no hay un valor establecido par este tipo de bomba")
+    return 1; 
+    var user = prompt("la bomba es una bomba de gasolina");
+    alert(user)
+    alert("la bomba es una bomba de agua");
+    return 2; 
+    var user = prompt("la bomba es una bomba de gasolina");
+    alert(user)
+    alert("la  bomba es una bomba de gasolina");
+    return 3; 
+    var user = prompt("la bomba es una bomba de hormigon");
+    alert(user)
+    alert("la bomba es una bomba de hormigon");
+    return 4; 
+    var user = prompt("la bomba es una bomba alimenticia");
+    alert(user)
+    alert("la bommba es una bomba de pasta alimenticia");
+    return ; 
+    var user = prompt("no hay un valor establecido par este tipo de bomba");
+    alert(user)
+    alert("No hay un valor establecido para este tipó de  bomba");
+
+}
+console.log (Tipo_motor)
